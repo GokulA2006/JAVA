d12cea9b06b29e5bc1386d68f88d152dd0bc7014
GokulA2006 <yokeshgokul8@gmail.com>
2026-08-11 11:41:26 +0530

    Add files via upload

---
+++ classesobject.java
@@ -0,0 +1,14 @@
+public class classesobject{
+
+
+    int coffee = 20;
+    int tea = 10;
+    public static void main(String args[]) {
+//creating anobject for a class
+classesobject server1= new classesobject();
+   System.out.println(server1.coffee);
+
+classesobject server2 = new classesobject();
+   System.out.println(server2.tea);
+    }
+}
