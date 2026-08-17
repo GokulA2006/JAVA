0d1e2cb69781ff1d85d878f52c329ad84e77e331
GokulA2006 <yokeshgokul8@gmail.com>
2026-08-17 21:54:17 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class hello{
+    public static void main(String args[])
+    {
+    Scanner scan = new Scanner(System.in);
+       String a = scan.nextLine();
+       String b = scan.nextLine();
+       System.out.print(a+b); 
+    }
+}
