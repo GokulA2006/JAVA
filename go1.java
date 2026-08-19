8249541be3d4e5e586c0f137baa218125d6847d5
GokulA2006 <yokeshgokul8@gmail.com>
2026-08-19 20:17:24 +0530

    Add files via upload

---
+++ go1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class go1{
+public static void main(String args[])
+{
+    Scanner scan = new Scanner(System.in);
+    int mark = scan.nextInt();
+
+    if(mark >= 35)
+    {
+        System.out.println("pass");
+    }
+    else{
+        System.out.println("fail");
+    }
+
+}
+}
